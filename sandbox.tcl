0ea1d8d70f6819460e4bff4038bab8cee1c539c3
seth <rader.seth@gmail.com>
2017-04-11 12:10:00 -0400

    Blocks move discretely

--- sandbox.tcl
+++ sandbox.tcl
@@ -22,21 +22,37 @@ grid columnconfigure . 0 -weight 1
 
 .workspace configure -scrollregion [list 0 0 640 480]
 
-set box [.workspace create rectangle 10 60 200 150 -fill black]
-.workspace bind $box <1> {selectElement $box %x %y}
-.workspace bind $box <B1-Motion> {moveElement %x %y}
+set box [.workspace create rectangle 20 20 40 40 -fill black -tags "element"]
+set box2 [.workspace create rectangle 20 60 40 80 -fill red -tags "element"]
+foreach b "$box $box2" {
+    .workspace bind $b <1> "selectElement $b %x %y"
+    .workspace bind $b <B1-Motion> "moveElement $b %x %y"
+    .workspace bind $b <B1-ButtonRelease> "dropElement $b"
+}
 
 proc selectElement {element mx my} {
-    set ::el $element
-    set ::initx $mx 
+    set ::initx $mx
+    set ::inity $my
+    .workspace raise $element 
+}
+
+proc moveElement {element mx my} {
+    .workspace move $element [expr $mx - $::initx] [expr $my - $::inity]
+    set ::initx $mx
     set ::inity $my
 }
 
-proc moveElement {mx my} {
-    if { [info exists ::el] } {
-        .workspace move $::el\
-                [expr $mx - $::initx] [expr $my - $::inity]
-        set ::initx $mx
-        set ::inity $my
+proc dropElement {element} {
+    set coords [.workspace coords $element]
+    for {set i 0} {$i < 2} {incr i} {
+        lset coords $i [expr {20 * round( [lindex $coords $i] / 20) }]
+        lset coords [expr $i + 2] [expr [lindex $coords $i] + 20]
     }
+    .workspace coords $element $coords
+    set bbox [.workspace bbox "element"]
+    for {set i 0} {$i < 2} {incr i} {
+        lset bbox $i [expr [lindex $bbox $i] - 20]
+        lset bbox [expr $i + 2] [expr [lindex $bbox [expr $i + 2]] + 20]
+    }
+    .workspace configure -scrollregion $bbox
 }
